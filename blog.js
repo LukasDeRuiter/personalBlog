a33df5eafc82c6c47fd403715997caf67e37efc3
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-05-05 20:16:52 +0200

    added js scroll feature that changes image src based on scrollpercentage

--- blog.js
+++ blog.js
@@ -24,6 +24,30 @@ document.getElementById("exitIcon1ContainerID").addEventListener('mouseleave', f
 
 })
 
+
+let userScrollLocation = 1;
+
 window.onscroll = function(){
-    console.log(Math.floor((window.scrollY / document.body.scrollHeight) * 100));
+    let scrollPercentage = Math.floor((window.scrollY / document.body.scrollHeight) * 100);
+    let image1 = document.getElementById("image1ID");
+    let image2 = document.getElementById("image2ID");
+    console.log(`yes ${userScrollLocation}`);
+    console.log(scrollPercentage);
+    if(scrollPercentage < 20 && userScrollLocation != 1){
+        image1.style.animation = "disappear 0.5s 1";
+        image1.onanimationend = function(){
+            image1.src = "article1Img1.png";
+            image1.style.animation = "appear 0.5s 1";
+        }
+        userScrollLocation = 1;
+    }
+    else if(scrollPercentage >= 20 && userScrollLocation != 2){
+        image1.style.animation = "disappear 0.5s 1";
+        image1.onanimationend = function(){
+            image1.src = "article1Img2.png";
+            image1.style.animation = "appear 0.5s 1";
+        }
+        userScrollLocation = 2;
+    
+    }
 }
